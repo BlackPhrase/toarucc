5b760d0936b569759fef39fee8a46b3696fabd19
BlackPhrase <BlackPhrase@users.noreply.github.com>
2020-01-15 12:53:52 +0000

    Stub linker app

---
+++ tcc-ld.c
@@ -0,0 +1,15 @@
+/* vim: tabstop=4 shiftwidth=4
+ * This file is part of the Toaru Compiler Collection (toarucc)
+ * and is licensed under the ISC license. See LICENSE for details.
+ *
+ * tcc-ld - linker app
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    fprintf(stderr, "hello world\n");
+    return EXIT_SUCCESS;
+};
